5e89f00873c8dc45e1511f2463332d7fa852c789
Bryan Wenger <bryanwenger@Bryans-MacBook-Pro.local>
2022-04-05 15:44:28 -0600

    Deliverable 3 R Script

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -18,3 +18,22 @@ Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFa
 total_summary <- Suspension_Coil %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), .groups = 'keep') 
 # Create a dataframe using group_by() and summarize() to group each lot by the mean, median, variance and standard devaition
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), .groups = 'keep') 
+
+
+#Deliverable 3
+#determine what arguments are needed to test against the PSI column across all lots
+?t.test()
+# determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(Suspension_Coil$PSI,mu=1500)
+#Subset Lot 1
+Lot1 <- subset(Suspension_Coil, Manufacturing_Lot== "Lot1")
+#Subset Lot 2
+Lot2 <- subset(Suspension_Coil, Manufacturing_Lot== "Lot2")
+#Subset Lot 3
+Lot3 <- subset(Suspension_Coil, Manufacturing_Lot== "Lot3")
+#T Test Lot 1
+t.test(Lot1$PSI,mu=1500)
+#T Test Lot 2
+t.test(Lot2$PSI,mu=1500)
+#T Test Lot 3
+t.test(Lot3$PSI,mu=1500)
